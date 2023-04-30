61976650838188036c3d272dfd46893b649859dd
onitsuyo <oo.smilyyyyy@gmail.com>
2023-04-30 17:40:41 +0900

    編集機能を実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,6 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update]
+
   def index
     @recipes = Recipe.all
   end
@@ -18,16 +20,25 @@ class RecipesController < ApplicationController
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
-    @ingredients = @recipe.ingredients
-    @seasonings = @recipe.seasonings
-    @procedures = @recipe.procedures
   end
 
   def edit
+    recipe_attributes = @recipe.attributes
+    @recipe_form = RecipeForm.new(recipe_attributes)
+    @recipe_form.ingredient_name = @recipe.ingredients.map(&:ingredient_name)
+    @recipe_form.seasoning_name = @recipe.seasonings.map(&:seasoning_name)
+    @recipe_form.step = @recipe.procedures.map(&:step)
   end
 
   def update
+    @recipe_form = RecipeForm.new(recipe_form_params)
+    @recipe_form.image ||= @recipe.image.blob
+    if @recipe_form.valid?
+      @recipe_form.update(recipe_form_params, @recipe)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
   
   private
@@ -35,4 +46,8 @@ class RecipesController < ApplicationController
   def recipe_form_params
     params.require(:recipe_form).permit(:recipe_title, :caption, :cooking_time, :ingredient_name, :seasoning_name, :step, :image).merge(user_id: current_user.id)
   end 
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
 end

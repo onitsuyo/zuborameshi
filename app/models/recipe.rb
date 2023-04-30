91ac23cdd31b7a22312aded5a2f9b8b15ac7160a
onitsuyo <oo.smilyyyyy@gmail.com>
2023-04-30 18:15:19 +0900

    料理を削除する機能を実装

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,11 +1,11 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  has_many :recipe_ingredients
+  has_many :recipe_ingredients, dependent: :destroy
   has_many :ingredients, through: :recipe_ingredients
-  has_many :recipe_seasonings
+  has_many :recipe_seasonings, dependent: :destroy
   has_many :seasonings, through: :recipe_seasonings
-  has_many :recipe_procedures
+  has_many :recipe_procedures, dependent: :destroy
   has_many :procedures, through: :recipe_procedures
 
   validates :recipe_title, null: false
